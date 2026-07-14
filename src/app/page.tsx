9070a4853a8cb961c40ba6bc6857980c830f0675
scheerfactor <st396@hotmail.com>
2026-07-14 04:32:38 -0400

    Expand Hosea 11 and 12 TSK coverage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,6 +54,7 @@ import tskHoseaSundaySchoolExpansion from "../../data/imports/tsk-hosea-4-9-revi
 import tskCrossWireHosea13Phase1 from "../../data/imports/tsk-crosswire-reviewed-hosea-13-phase-1.json";
 import tskCrossWireHosea13To14Phase2 from "../../data/imports/tsk-crosswire-reviewed-hosea-13-14-phase-2.json";
 import tskCrossWireHosea12To14Phase3 from "../../data/imports/tsk-crosswire-reviewed-hosea-12-14-phase-3.json";
+import tskCrossWireHosea11To12Phase4 from "../../data/imports/tsk-crosswire-reviewed-hosea-11-12-phase-4.json";
 import tskWeakBooksPhase1 from "../../data/imports/tsk-metav-reviewed-weak-books-phase-1.json";
 import tskWeakBooksPhase2 from "../../data/imports/tsk-metav-reviewed-weak-books-phase-2.json";
 import tskWeakBooksPhase3 from "../../data/imports/tsk-openbible-reviewed-weak-books-phase-3.json";
@@ -10139,6 +10140,7 @@ const additionalReviewedTskRows: TskCrossReferenceImportRow[] = [
   ...(tskCrossWireHosea13Phase1 as TskCrossReferenceImportRow[]),
   ...(tskCrossWireHosea13To14Phase2 as TskCrossReferenceImportRow[]),
   ...(tskCrossWireHosea12To14Phase3 as TskCrossReferenceImportRow[]),
+  ...(tskCrossWireHosea11To12Phase4 as TskCrossReferenceImportRow[]),
   ...(tskWeakBooksPhase14 as TskCrossReferenceImportRow[]),
   ...(tskWeakBooksPhase15 as TskCrossReferenceImportRow[]),
   ...(tskWeakBooksPhase16 as TskCrossReferenceImportRow[]),
